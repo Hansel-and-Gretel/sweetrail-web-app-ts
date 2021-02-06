063ffa802f1689b8f823845a783e470a380fc550
Chanhee Jeong <chan420420@gmail.com>
2021-02-07 03:14:47 +0900

    route 기본

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import MainPage from "./pages/main";
+import {
+    Switch,
+    Route,
+    Redirect,
+    useLocation,
+    BrowserRouter
+} from 'react-router-dom'
+
+
+function AppRoute() {
+
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={MainPage}/>
+                </Switch>
+            </BrowserRouter>
+        )
+}
+
 
 function App() {
   return (
-    <></>
+      <AppRoute/>
   );
 }
 
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function MainPage() {
+    return(
+        <>Sweetrail 메인페이지입니다.</>
+    )
+}
+
+export default MainPage;
