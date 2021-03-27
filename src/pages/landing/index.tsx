6b60748fca0f701ce81eafc9a8c9389abd28bc85
Chanhee Jeong <chan420420@gmail.com>
2021-03-27 23:12:44 +0900

    add Landing Page

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,105 +1,373 @@
 import React from 'react'
 import styled from "styled-components";
 import Navbar from "../../components/common/Nav/Navbar";
+import * as colors from './../../../src/styles/colors'
+import artwork from "../../assets/illust/dongle.png";
+import BasicButton from "../../components/common/Button";
 
 const S = {
     Container: styled.div`
-      display: block;
-      width: 100%;
-      height: 100vh;
-      background: linear-gradient(to bottom,
-      rgba(20, 20, 20, 0) 10%,
-      rgba(20, 20, 20, 0.25) 25%,
-      rgba(20, 20, 20, 0.5) 50%,
-      rgba(20, 20, 20, 0.75) 75%,
-      rgba(20, 20, 20, 1) 100%), url('https://images.unsplash.com/photo-1426086800127-2601510ca027?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80');
-      background-size: cover;
-      @media (min-width: 1200px) {
+        display: block;
+        padding: 0 5%;
+    `,
+}
+
+
+const FirstSection = {
+    Container: styled.div  `
         display: inline-flex;
+        height: 100vh;
+      @media screen and (max-width: 768px) { //tablet
+       height: 500px;
+      }
+      @media screen and (max-width: 356px) { //mob
+        height: 400px;
       }
     `,
-    TitleContainer: styled.div`
-      display: block;
-      height: 25%;
-      margin-bottom: 30px;
-      div {
-        padding-top: 20%;
-        padding-left: 5%;
-        font-size: 3rem;
+    LeftContainer: styled.div`
+      margin-left: 100px;
+      p{
+        margin-top: 200px;
         font-weight: 800;
-        color: white;
-
-        p {
-          font-weight: 600;
+        font-size: 4rem;
+        .second-line{
+          font-weight: normal;
           font-size: 1rem;
-          color: white;
           margin-top: 10px;
+          color: ${colors.gray55};
         }
       }
-     
-      @media (min-width: 768px) {
-        div {
-          padding-top: 10%;
-          font-size: 4rem;
+      @media screen and (max-width: 768px) { //tablet
+        margin-left: 35px;
+        p {
+          margin-top: 100px;
+          font-size: 2rem;
         }
       }
-      @media (min-width: 1200px) {
-        width: 50%;
-        padding-top: 30px;
-        padding-left: 60px;
-        float: left;
-        div {
-          font-size: 4rem;
-          padding-top: 40%;
-          padding-left: 200px;
-          
-        }
+    `,
+    ButtonContainer: styled.div`
+      display: block;
+      color: white;
+      width: 300px;
+      margin-top: 30px;
+      @media screen and (max-width: 768px) { //tablet
+        width: 135px;
+        margin-top: 15px;
       }
-      
       
     `,
-    FormContainer: styled.div`
-      display: block;
-      background: white;
-      margin: 0 auto;
-      border-radius: 10px;
-      width: 90%;
-      height: 65%;
-      @media (min-width: 768px) {
-        width: 50%;
+    Background: styled.div`
+      img{
+        margin-top: 90px;
+        position: absolute;
+        right: 0;
+        width: 40%;
+        z-index: -1;
       }
-      @media (min-width: 1200px) {
-        width: 30%;
-        height: 80%;
-        float: right;
-        margin: 70px auto;
+      @media screen and (max-width: 768px) { //tablet
+        margin-top: 50px;
       }
       
+    `
+
+}
+
+const SecondSection = {
+    Container: styled.div  `
+      margin-top: 50px;
+      text-align: center;
+      margin-bottom: 50px;
+    `,
+    Title: styled.div`
+      font-size: 48px;
+      font-weight: 800;
+      @media screen and (max-width: 768px) { //tablet
+        font-size: 24px;
+      }
+    `,
+    Description: styled.div`
+      margin: 16px 0 32px 0;
+      color: ${colors.gray90};
+      padding: 0 10%;
+    `,
+    CardDeck: styled.div`
+      display: inline-block;
+      margin-top: 10px;
+    `,
+    Card: styled.div`
+      display: inline-block;
+      margin: 10px;
+      width: 400px;
+      height: 340px;
+      border-radius: 8px;
+      background: ${colors.white};
+      box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+      
+      
+      div{
+        margin: 25% auto 0;
+        background: #F3F0EA;
+        width: 66px;
+        height: 66px;
+      }
+      h3 {
+        margin-top: 20px;
+        font-size: 22px;
+        font-weight: bold;
+      }
+      p{
+        width: 80%;
+        margin: 10px auto;
+      }
+
+      @media screen and (max-width: 768px) { //mob
+        width: 50%;
+        height: 80%;
+        div{
+          margin: 5% auto 0;
+          background: #F3F0EA;
+          width: 33px;
+          height: 33px;
+        }
+        h3 {
+          margin-top: 10px;
+          font-size: 16px;
+          font-weight: bold;
+        }
+        p{
+          width: 80%;
+          margin: 5px auto 25px;
+        }
+      }
+    `
+
+
+}
+
+const ThirdSection = {
+    Container: styled.div  `
+      margin: 200px 100px;
+      @media screen and (max-width: 768px) { //tablet
+        padding: 10px;
+        margin: 50px 20px;
+      }
+    `,
+    RowItem: styled.div`
+      display: inline-block;
+    `,
+    TextContainer: styled.div`
+      width: 600px;
+      height: 360px;
+      text-align: left;
+      padding: 16px;
+      h1 {
+        font-size: 2rem;
+        font-weight: 800;
+      }
+      p{
+        margin-top: 16px;
+      }
+      @media screen and (max-width: 768px) { //tablet
+        width: 100%;
+        padding: 0;
+        margin-bottom: 0;
+      }
     `,
 
+
+}
+
+const FourthSection = {
+    Container: styled.div  `
+      margin-top: 50px;
+      text-align: center;
+      margin-bottom: 50px;
+    `,
+    Title: styled.div`
+      font-size: 48px;
+      font-weight: 800;
+      @media screen and (max-width: 768px) { //tablet
+        font-size: 24px;
+      }
+    `,
+    Description: styled.div`
+      margin: 16px 0 32px 0;
+      color: ${colors.gray90};
+      padding: 0 10%;
+    `,
+    CardDeck: styled.div`
+      display: inline-block;
+      margin-top: 10px;
+    `,
+    Card: styled.div`
+      display: inline-block;
+      margin: 10px;
+      width: 300px;
+      height: 200px;
+      border-radius: 8px;
+      background: ${colors.white};
+      box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+      text-align: left;
+      padding: 30px;
+      
+      h3 {
+        font-size: 22px;
+        font-weight: bold;
+        margin-bottom: 16px;
+      }
+
+      @media screen and (max-width: 768px) { //mob
+        width: 80%;
+        height: 80%;
+       
+        h3 {
+          font-size: 16px;
+          font-weight: bold;
+        }
+        p{
+          width: 80%;
+        }
+      }
+    `
+
+
+}
+
+const FifthSection ={
+    Container: styled.div  `
+      margin: 50px auto;
+      text-align: center;
+      
+    `,
+    Title: styled.div`
+      font-size: 36px;
+      font-weight: 800;
+      @media screen and (max-width: 768px) { //tablet
+        font-size: 20px;
+      }
+    `,
+    Description: styled.div`
+      margin: 16px auto 32px auto;
+      color: ${colors.gray90};
+      padding: 0 10%;
+    `,
+    ButtonContainer: styled.div`
+      display: block;
+      color: white;
+      width: 300px;
+      margin: 10px auto ;
+      @media screen and (max-width: 768px) { //tablet
+        width: 135px;
+      }
+      
+    `,
 }
 
 function LandingPage() {
 
 
     return (
-        <Navbar/>
-        // <S.Container>
-        //     <S.TitleContainer>
-        //         <div>Sweetrail
-        //             <p>나의 한걸음 한걸음을 기록하고 </p>
-        //             <p>누군가와 일상, 여행, 관심을 공유하고 싶나요?</p>
-        //         </div>
-        //     </S.TitleContainer>
-        //     <S.FormContainer>
-        //         <form>
-        //             <h2>Login</h2>
-        //             <input type="email" id="inputEmail" className="form-control" placeholder="이메일 주소" name="email" onChange={()=>{}}/>
-        //             <input type="email" id="inputEmail" className="form-control" placeholder="이메일 주소" name="email" onChange={()=>{}}/>
-        //             <button>버튼</button>
-        //         </form>
-        //     </S.FormContainer>
-        // </S.Container>
+        <>
+            <Navbar/>
+            <S.Container>
+
+                <FirstSection.Container>
+                    <FirstSection.LeftContainer>
+                        <p>Pin your Passion<br/>on your Path
+                            <p className='second-line'>Now, you don’t need to record all your path manually. </p>
+                        </p>
+                        <FirstSection.ButtonContainer>
+                            <BasicButton theme='red'>Start Trail</BasicButton>
+                        </FirstSection.ButtonContainer>
+                    </FirstSection.LeftContainer>
+                    <FirstSection.Background>
+                        <img src={artwork} alt="artwork"/>
+                    </FirstSection.Background>
+                </FirstSection.Container>
+
+                <SecondSection.Container>
+                    <SecondSection.Title>Trail is always with you</SecondSection.Title>
+                    <SecondSection.Description>
+                        Trail is the best app for all travelers in the world.
+                        <br/>
+                        Our service will make you experience the convenience of travelling and journalling!
+                    </SecondSection.Description>
+                    <SecondSection.CardDeck>
+                        <SecondSection.Card>
+                            <div>icon</div>
+                            <h3>Location Tracking</h3>
+                            <p>Trail record your journey(trail) on background~~~</p>
+                        </SecondSection.Card>
+                        <SecondSection.Card>
+                            <div>icon</div>
+                            <h3>Location Tracking</h3>
+                            <p>Trail record your journey(trail) on background~~~</p>
+                        </SecondSection.Card>
+                        <SecondSection.Card>
+                            <div>icon</div>
+                            <h3>Location Tracking</h3>
+                            <p>Trail record your journey(trail) on background~~~</p>
+                        </SecondSection.Card>
+                    </SecondSection.CardDeck>
+                </SecondSection.Container>
+
+                <ThirdSection.Container>
+                    <ThirdSection.RowItem>
+                        <ThirdSection.TextContainer>
+                            <h1>Kickstart an organization with your co-founders</h1>
+                            <p>
+                                It’s the early days, you just had a long conversation with two friends about a meaningful challenge that you’re all passionate about and have a potential solution for. You’re ready to embark the startup journey.
+                            </p>
+                        </ThirdSection.TextContainer>
+                        <ThirdSection.TextContainer>
+                            <h1>Kickstart an organization with your co-founders</h1>
+                            <p>
+                                It’s the early days, you just had a long conversation with two friends about a meaningful challenge that you’re all passionate about and have a potential solution for. You’re ready to embark the startup journey.
+                            </p>
+                        </ThirdSection.TextContainer>
+                    </ThirdSection.RowItem>
+                </ThirdSection.Container>
+
+                <FourthSection.Container>
+                    <FourthSection.Title>Reviews</FourthSection.Title>
+                    <FourthSection.Description>
+                        A lot of people around the world satisfied with Trail ! <br/>
+                        Check how they use our service and make trail yours :)
+                    </FourthSection.Description>
+                    <FourthSection.CardDeck>
+                        <FourthSection.Card>
+                            <h3>Location Tracking</h3>
+                            <p>Trail record your journey(trail) on background~~~</p>
+                        </FourthSection.Card>
+                        <FourthSection.Card>
+                            <h3>Location Tracking</h3>
+                            <p>Trail record your journey(trail) on background~~~</p>
+                        </FourthSection.Card>
+                        <FourthSection.Card>
+                            <h3>Location Tracking</h3>
+                            <p>Trail record your journey(trail) on background~~~</p>
+                        </FourthSection.Card>
+                        <FourthSection.Card>
+                            <h3>Location Tracking</h3>
+                            <p>Trail record your journey(trail) on background~~~</p>
+                        </FourthSection.Card>
+                    </FourthSection.CardDeck>
+                </FourthSection.Container>
+
+                <FifthSection.Container>
+                    <FifthSection.Title>Get 'Trail'</FifthSection.Title>
+                    <FifthSection.Description>
+                        If you can’t wait to start your own trail or go somewhere <br/>
+                        without knowing nothing, please click the button below asap
+                    </FifthSection.Description>
+                    <FifthSection.ButtonContainer>
+                        <BasicButton theme='red'>Download App ( Android )</BasicButton>
+                    </FifthSection.ButtonContainer>
+                </FifthSection.Container>
+
+            </S.Container>
+        </>
+
     )
 }
 
